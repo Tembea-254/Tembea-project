2f078ce05d686d6df6cfe7fc039cdad05307ed59
wakoTheDev <wakopaul19@gmail.com>
2025-09-03 23:58:22 +0300

    updated

---
+++ spinner.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.getElementById('spinner-overlay').style.display = 'none';
+    },1500); // Spinner stays for 1.5 seconds
+// });
